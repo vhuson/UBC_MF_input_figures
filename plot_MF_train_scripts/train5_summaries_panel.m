6da16ab15f371bacc9810b02745e6ed52abed377
Vincent Huson <v.huson@gmail.com>
2024-02-28 14:50:18 -0500

    Fig 6 smooth pursuit trains: more possible summary panels

--- plot_MF_train_scripts/train5_summaries_panel.m
+++ plot_MF_train_scripts/train5_summaries_panel.m
@@ -2,13 +2,13 @@ f_train = figure('Position', [488 1.8000 680.3150 857.9636],...
     'Color','w');
 
 %Define axis position
-num_cols = 4;
+num_cols = 3;
 num_rows = 1;
 
-left_margin = 0.08;
+left_margin = 0.1;
 bottom_margin = 0.08;
-base_width = 0.14;
-base_space = 0.09;
+base_width = 0.18;
+base_space = 0.14;
 
 graph_height = 0.14;
 
@@ -16,23 +16,52 @@ all_left_edges = (base_width + base_space) .* (0:(num_cols-1)) + left_margin;
 
 
 %Get data
+% all_plot_par = {all_train_slow_amp,...
+%                     all_train_n_spikes,...
+%                     all_train_slow_HD,...
+%                     all_train_pause};
+
+% all_plot_par = {all_train_slow_amp,...
+%                     all_train_n_spikes,...
+%                     all_n_spikes_stim,...
+%                     all_n_spikes_post};
+
 all_plot_par = {all_train_slow_amp,...
-                    all_train_n_spikes,...
-                    all_train_slow_HD,...
-                    all_train_pause};
+                    all_n_spikes_stim,...
+                    all_n_spikes_post};
+
+% all_plot_par = {all_train_slow_amp,...
+%                     all_train_n_spikes,...
+%                     all_train_burst_baseline(2:end)};
 
 %Data specific options
-all_ylabels = {'Peak response (\Deltaspk/s)',...
-                "Response spikes (n)",...
-                'Half-width (s)',...
-                'Pause (s)'};
+% all_ylabels = {'Peak response (\Deltaspk/s)',...
+%                 "Response spikes (n)",...
+%                 'Half-width (s)',...
+%                 'Pause (s)'};
 
-all_min_vals = [1 1 -Inf -Inf];
+% all_ylabels = {'Peak response (\Deltaspk/s)',...
+%                 "Response spikes (n)",...
+%                 'Stim spikes (n)',...
+%                 'Post spikes (n)'};
+% 
+all_ylabels = {'Peak response (\Deltaspk/s)',...
+                'Stim spikes (n)',...
+                'Post spikes (n)'};
+
+% all_ylabels = {'Peak response (\Deltaspk/s)',...
+%                 "Response spikes (n)",...
+%                 'Steady state (spk/s)'};
+
+
+% all_min_vals = [1 1 0.01];
+all_min_vals = [1 -Inf -Inf];
 
 
 
 %Train plot pars
-plot_steps = 1:6;
+% plot_steps = 1:6;
+plot_steps = [2, 7, 8];
 step_size = [10, 20, 30, 40, 50, 60, 20, 20];
 
 
@@ -40,17 +69,17 @@ step_size = [10, 20, 30, 40, 50, 60, 20, 20];
 summary_on = fltr_ONidx_t5;
 % summary_off = OFFidx(end-1:end);
 summary_off = [];
-
+    
 
 opts = struct();
-opts.input_n = step_size(plot_steps);
-% opts.input_n = [0 1 2.5 5];
+% opts.input_n = step_size(plot_steps);
+opts.input_n = [1 2 3];
 opts.YScale = 'log';
 % opts.XScale = 'log';
 opts.XLabel = "Input step (spk/s)";
 
 opts.XTick = opts.input_n;
-opts.XTickLabel = opts.input_n;
+opts.XTickLabel = step_size(plot_steps);
 opts.min_val = 1;
 
 
@@ -73,6 +102,7 @@ for p_idx = 1:num_cols
     %Offset XLim a little bit
     ax_train_par{p_idx}.XLim(1) = ax_train_par{p_idx}.XLim(1)...
         - diff(ax_train_par{p_idx}.XLim) * 0.05;
-
-    fix_powered_ylabels(ax_train_par{p_idx})
+    % fix_powered_ylabels(ax_train_par{p_idx})
+    opts.YScale = 'linear';
 end
+fix_powered_ylabels(ax_train_par{1})
