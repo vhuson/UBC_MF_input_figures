3014bddc37e3b1f0d95b7a2d2613cf61ebddba4d
Vincent Huson <v.huson@gmail.com>
2024-07-18 13:51:32 -0400

    Tweaked function to allow it to be used combined with hold on calls

--- functions/plot_funs/plot_fs.m
+++ functions/plot_funs/plot_fs.m
@@ -1,4 +1,4 @@
-function [] = plot_fs(yData,Fs,curr_fig)
+function [] = plot_fs(yData,Fs,curr_ax)
 %UNTITLED2 Summary of this function goes here
 %   Detailed explanation goes here
 if nargin < 2
@@ -7,11 +7,12 @@ end
 
 if nargin < 3
     curr_fig = figure;
+    curr_ax = axes(curr_fig);
 end
 xData = 1:numel(yData);
 xData = xData/Fs;
 
-curr_ax = axes(curr_fig);
+
 plot(curr_ax,xData,yData);
 
 end
